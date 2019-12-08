5bcac5d7deadcc824ac30de9ecf30ce00d173c6a
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2019-12-08 20:20:23 +0530

    update README

--- src/config/utils.ts
+++ src/config/utils.ts
@@ -4,8 +4,8 @@ export const ENTER_KEY = 13
 
 export const uuidv4 = () => {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    let r = (Math.random() * 16) | 0
-    let v = c === 'x' ? r : (r & 0x3) | 0x8
+    const r = (Math.random() * 16) | 0
+    const v = c === 'x' ? r : (r & 0x3) | 0x8
     return v.toString(16)
   })
 }
