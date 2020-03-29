619118ae120828b07996a9bf6c0ff6cf5c7795a1
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2020-03-30 01:06:47 +0530

    replace concurrently

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios, { AxiosRequestConfig } from 'axios'
+import { SWRConfig } from 'swr'
 import './App.css'
 import { AddTodo } from 'components/AddTodo'
 import { TodoList } from 'components/TodoList'
@@ -6,12 +8,19 @@ import { UpdateAppToast } from 'components/UpdateAppToast'
 
 export const App: React.FC = () => {
   return (
-    <>
+    <SWRConfig
+      value={{
+        fetcher: async (...args: [string, AxiosRequestConfig]) => {
+          const res = await axios(...args)
+          return res.data
+        },
+      }}
+    >
       <UpdateAppToast />
       <div className="todoapp">
         <AddTodo />
         <TodoList />
       </div>
-    </>
+    </SWRConfig>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import axios, { AxiosRequestConfig } from 'axios'
-import { SWRConfig } from 'swr'
+
 import 'todomvc-app-css/index.css'
 
 import 'index.css'
 import { App } from 'App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-  <SWRConfig
-    value={{
-      fetcher: async (...args: [string, AxiosRequestConfig]) => {
-        const res = await axios(...args)
-        return res.data
-      },
-    }}
-  >
-    <App />
-  </SWRConfig>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
